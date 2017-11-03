8dbc7f08269fa28ede66c44827b415e76f9481bc
astefanovic <astefanovic101@gmail.com>
2017-11-03 16:54:03 +1100

    Added loop to install all packages

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
 (require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
   (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+  
 (package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -15,23 +16,31 @@
  '(explicit-shell-file-name "C:\\Windows\\System32\\bash.exe")
  '(package-selected-packages
    (quote
-    (rjsx-mode zenburn-theme google-this magit treemacs json-mode web-beautify emmet-mode web-mode))))
+    (rjsx-mode zenburn-theme google-this treemacs json-mode web-beautify emmet-mode web-mode))))
+
+;; Ensures packages are installed
+(dolist (package '(rjsx-mode zenburn-theme google-this treemacs json-mode web-beautify emmet-mode web-mode))
+ (unless (package-installed-p package)
+   (package-install package))
+   (require package))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ 
 ;; User added modes below
 ;; This shit dont work for windows
 ;;(add-to-list 'default-frame-alist
 ;;                       '(font . "Source Code Pro-12"))
 (setf (alist-get "\\.js\\'" auto-mode-alist) 'rjsx-mode)
-(google-this-mode 1)
 (load-theme 'zenburn t)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 (desktop-save-mode 1)
+
 ;; Sets the shell to bash
 (setenv "PID" nil)
 (defun my-shell-setup ()
