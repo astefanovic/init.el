bcf81c3b6de6a7bf3d1020dd74043284ea4f1a2e
astefanovic <astefanovic101@gmail.com>
2017-11-04 23:41:18 +1100

    Added file extensions for auto web-mode

--- init.el
+++ init.el
@@ -41,6 +41,16 @@
 (scroll-bar-mode -1)
 (desktop-save-mode 1)
 
+;; Web-mode modes
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+
 ;; Sets the shell to bash
 (setenv "PID" nil)
 (defun my-shell-setup ()
